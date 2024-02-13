3dcf7986004d2772eb80ae17b9d412e7de76b784
squidmin <morse.james.r@gmail.com>
2024-02-13 17:12:53 -0600

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { ThemeProvider } from './ThemeContext'; // Adjust the import path as necessary
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component tests', () => {
+  test('renders XML to JSON Converter text in all expected places', () => {
+    render(
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    );
+    const linkElements = screen.getAllByText(/XML to JSON Converter/i);
+    expect(linkElements.length).toBeGreaterThan(0); // Adjust this based on the expected count
+    linkElements.forEach(element => {
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+  test('toggles theme from light to dark', () => {
+    render(
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    );
+
+    const heading = screen.getByRole('heading', { name: /XML to JSON Converter/i });
+    expect(heading).toBeInTheDocument();
+  });
 });
